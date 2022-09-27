2e0f731624bd59872a3206bd6710d12566ca643e
youngcoder205 <69458400+youngcoder205@users.noreply.github.com>
2022-09-27 14:47:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+x = 0;
+y = 0;
+draw_circle = ""
+draw_rectangle = ""
+
+var speechrecognition = window.webkitSpeechRecognition
+var recognition = new speechrecognition()
+
+function start()  {
+    document.getElementById("status").innerHTML = "system is listening please speak"
+    recognition.start()
+}
+recognition.onresult = function(event) {
+    console.log (event)
+    var content = event.results[0][0].transcript
+    if (content == "circle") {
+        x = Math.floor(Math.random()*900)
+        y = Math.floor(Math.random()*600)
+        draw_circle = "set"
+    }
+    if (content == "rectangle") {
+        x = Math.floor(Math.random()*900)
+        y = Math.floor(Math.random()*600)
+        draw_rectangle = "set"
+    }
+}
+
+function setup() {
+    canvas = createCanvas(900,600)
+}
+
+function draw()  {
+    if (draw_circle == "set") {
+        radius = Math.floor(Math.random()*100)
+        circle(x,y,radius)
+        document.getElementById("status").innerHTML = "circle is drawn"
+        draw_circle = ""
+
+        if (draw_rectangle == "set"){
+            rect(x,y,70,50)
+            document.getElementById("status").innerHTML = "Rectangle is drawn"
+            draw_rectangle = ""
+        }
+    }
+}
